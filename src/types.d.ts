a96f0507d0602743eeef1ad9b990217c9f194939
littlekidz <worasretkulkit@gmail.com>
2025-12-13 14:29:09 +0700

    Initial commit - developer portfolio

---
+++ src/types.d.ts
@@ -0,0 +1,10 @@
+// useful small types for projects
+export type Project = {
+  id: string;
+  title: string;
+  subtitle?: string;
+  desc: string;
+  tech: string[];
+  link?: string;
+  repo?: string;
+};
